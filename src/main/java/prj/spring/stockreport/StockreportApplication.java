ee07c1be938858c141ee9d718947e58d6c030cd4
aymaneiml <aymaneimlihi@gmail.com>
2025-09-25 19:18:00 +0200

    initial commit

---
+++ src/main/java/prj/spring/stockreport/StockreportApplication.java
@@ -0,0 +1,13 @@
+package prj.spring.stockreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prj/spring/stockreport/StockreportApplicationTests.java
@@ -0,0 +1,13 @@
+package prj.spring.stockreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
